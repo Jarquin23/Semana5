fd388216b4e0622d03ae7fa4e27fd2edf6d2c213
Jarquin23 <amjarquinc@uamv.edu.ni>
2025-04-22 11:51:20 -0600

    Ejercicio1

---
+++ Actividad4/ejercicio1.py
@@ -0,0 +1,7 @@
+inicio = int(input("Dime el valor inicial: "))
+fin = int(input("Dime el valor final: "))
+num = int(input("Dime un numero: "))
+if num >= inicio and num <= fin:
+    print("El numero es valido")
+else:
+    print("El numero no es valido")
