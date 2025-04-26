5c8c58aad7dd9bf392de8c551b7c768bed0b9ee2
Jarquin23 <amjarquinc@uamv.edu.ni>
2025-04-25 19:19:57 -0600

    Ejercicio3 calculo de dia de la semana

---
+++ sesion_viernes/ejercicio3.py
@@ -0,0 +1,17 @@
+print("Presiona Cero para salir")
+dia = int(input("Ingrese el día de la semana de forma numérica(1-7)"))
+while dia !=0:
+    if dia==1:
+        print("Lunes")
+    if dia==2:
+        print("Martes")
+    if dia==3:
+        print("Miércoles")
+    if dia==4:
+        print("Jueves")
+    if dia==5:
+        print("Viernes")
+    if dia==6:
+        print("Sábado")
+    if dia==7:
+        print("Domingo")
