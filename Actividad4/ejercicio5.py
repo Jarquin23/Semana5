cb117b4aa0ef843a94f52ddf38439a4f7eec1a03
Jarquin23 <amjarquinc@uamv.edu.ni>
2025-04-25 19:52:09 -0600

    Ejercicio5 verificación números

--- Actividad4/ejercicio5.py
+++ Actividad4/ejercicio5.py
@@ -1,5 +1,7 @@
-edad = int(input("Dime tu edad: "))
-if edad >=18:
-    print("Puede votar")
+num = int(input("Dime un número: "))
+if num>0:
+    print("El número es positivo")
+if num % 2 == 0:
+    print("El número es par")
 else:
-    print("No puede votar")
+    print("El número es impar")
