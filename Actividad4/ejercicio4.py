3a5c12ffed8fa4f77df52c9587ae005929fb3a87
Jarquin23 <amjarquinc@uamv.edu.ni>
2025-04-25 19:41:49 -0600

    Ejercicio4 calcular edad para volar

--- Actividad4/ejercicio4.py
+++ Actividad4/ejercicio4.py
@@ -1,5 +1,5 @@
 edad = int(input("Ingrese su edad: "))
 if edad >= 18:
-    print("Puede volar.")
+    print("Puede votar.")
 else:
-    print("No tiene la edad adecuada para volar.")
+    print("No tiene la edad adecuada para votar.")
---
+++ Actividad4/ejercicio5.py
@@ -0,0 +1,5 @@
+edad = int(input("Dime tu edad: "))
+if edad >=18:
+    print("Puede votar")
+else:
+    print("No puede votar")
