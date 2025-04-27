700f05ea714974470a4a3c2e14417259ccf3c71a
Jarquin23 <amjarquinc@uamv.edu.ni>
2025-04-26 20:47:16 -0600

    Ejercicio9 encontrar el mayor de 3 numeros

--- Actividad4/ejercicio9.py
+++ Actividad4/ejercicio9.py
@@ -1,17 +1,17 @@
 num1 = float(input("Dime un número: "))
 num2 = float(input("Dime otro número: "))
 num3 = float(input("Dime el último número: "))
-if num1==num2==num3:
+if num1 == num2 == num3:
     print("Los números son iguales")
 else:
-    if num1>=num2:
-        if num1>=num3;
-        mayor=num1
+    if num1 >= num2:
+        if num1 >= num3:
+            mayor = num1
         else:
-        mayor=num3
+            mayor = num3
     else:
-        if num2>=num3:
-            mayor=num2
+        if num2 >= num3:
+            mayor = num2
         else:
-            mayor=num3
+            mayor = num3
     print(f"El número mayor es: {mayor}")
